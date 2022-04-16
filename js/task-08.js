cbfa25449f23a79287587d3359a43df502e29a68
Yevhenii Kononets <yudzhin1408@gmail.com>
2022-04-16 16:57:10 +0300

    task8,9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    
+     if (form.elements.email.value === '' || form.elements.password.value==='') {
+         alert('Все поля должны быть заполнены!!!');
+         return;
+}
+    formData.forEach((value, name) => {
+      
+        console.log(name, ':', value);
+    });
+    event.currentTarget.reset();
+
+};
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+
+const button = document.querySelector('.change-color');
+const text = document.querySelector('.color');
+const body = document.querySelector('body');
+
+button.addEventListener('click', backgroundСolor);
+
+function backgroundСolor() {
+  body.style.backgroundColor = getRandomHexColor() 
+  text.textContent = getRandomHexColor();  
 }
