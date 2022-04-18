9cd1b4fbc22cbcaa7f08562d093ffe96c6e65797
Yevhenii Kononets <yudzhin1408@gmail.com>
2022-04-18 12:13:01 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,7 @@
 const inputEl = document.querySelector('#name-input');
 const nameEl = document.querySelector('#name-output');
 
-inputEl.addEventListener('keyup', function (meaning) {
+inputEl.addEventListener('input', function (meaning) {
     nameEl.textContent = meaning.target.value;
     if (!inputEl.value) {
     nameEl.textContent ='Anonymous';
